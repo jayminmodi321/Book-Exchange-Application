29ab6d4785a6f9f057b134345d3000d749ed55a5
jaymin7051 <jaymin.modi@einfochips.com>
2024-05-14 11:36:38 +0530

    Book Exchange Commit

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,7 @@ app.use(cors());
 
 app.get("/", (request, response) => {
   console.log(request);
-  return response.status(234).send("Welcome to MERN Stack Book Shop");
+  return response.status(234).send("Welcome to BookExchange App");
 });
 
 app.use("/books", booksRoute);
